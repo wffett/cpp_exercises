24c3f9ed599e1a2a5d5e9376f0a8557ac3b0c984
wffett <wangffett@gmail.com>
2018-11-21 12:43:14 +0800

    first

---
+++ ComputeArea.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double radius;
+	double area;
+	radius=20;
+	area=radius*radius*3.1415;
+	cout << "The area is :\n" <<
+			area << endl;
+	return 0;
+		}
