cd8be4c28c329ce3ebd1b3f5e924e92e87f50a48
wffett <wangffett@gmail.com>
2019-01-23 23:48:21 +0800

    updata mmake.cpp

--- mmake.cpp
+++ mmake.cpp
@@ -1,24 +1,32 @@
+#include <iostream>
 #include <fstream>
+#include <string>
+
 using namespace std;
 
-void make_main(char *fun_name[]);
-void make_makefile(char *file_name[]);
+void make_main(string funname);
+//void make_makefile(char *file_name[]);
 
 
 int main(int argc , char *argv[])
 {
-	ofstream out(argv[1]);
-	out << "#include <iostream>\n";
-	out << endl;
-	out.close();
-
+	make_main(argv[1]);
 	return 0;
 }
 
-void make_main(char *fun_name[])
+void make_main(string funname)
 {
-	ofstream out_main;	
-	out_main.open("");
-	
+	int dotnum=funname.find_last_of(".");
+	funname = funname.substr(0,dotnum);
+	ofstream out("main.cpp");
+	out << "#include <iostream>\n";
+	out << "using namespace std;\n";
+	out << "void " << funname << "()" << ";\n";
+	out << "int main(){\n";
+	out << funname << "()" << ";\n";
+	out << "return 0;" << endl;
+	out << "}";
+	out << endl;
+	out.close();
 	
 }
